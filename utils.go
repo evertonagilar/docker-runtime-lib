1bb7f3567e48f3ae6fbbcb13346d3c8b505a13db
Everton de Vargas Agilar <evertonagilar@gmail.com>
2025-10-22 14:19:15 -0300

    v1.0.11

--- utils.go
+++ utils.go
@@ -1,6 +1,10 @@
 package container
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
 
 func QuoteList(list []string) []string {
 	quoted := make([]string, len(list))
@@ -8,4 +12,38 @@ func QuoteList(list []string) []string {
 		quoted[i] = fmt.Sprintf("%q", s)
 	}
 	return quoted
+}
+
+func CheckKubectlAvailable() error {
+	cmd := exec.Command("kubectl", "version", "--client")
+	if output, err := cmd.Output(); err != nil {
+		return fmt.Errorf("kubectl não está instalado")
+	} else {
+		_ = strings.TrimSpace(string(output))
+	}
+	return nil
+}
+
+func CheckK8sContextAvailable() error {
+	cmd := exec.Command("kubectl", "config", "current-context")
+	output, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("kubectl sem contexto configurado. Use 'kubectl config use-context <nome>'")
+	}
+	ctx := strings.TrimSpace(string(output))
+	if ctx == "" || strings.EqualFold(ctx, "none") {
+		return fmt.Errorf("nenhum contexto Kubernetes selecionado no kubectl")
+	}
+	return nil
+}
+
+func CheckKubernetesAvailable() error {
+	if err := CheckKubectlAvailable(); err != nil {
+		return err
+	}
+	if err := CheckK8sContextAvailable(); err != nil {
+		return err
+	}
+
+	return nil
 }
